5a688199b70bc671255dfc6e513777d6d0d23243
Administrator <Administrator@CHN-YONGSHENG>
2016-12-12 15:53:54 +0800

    Add project files.

---
+++ Kosmos.LogServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Kosmos.LogServer.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Kosmos.LogServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kosmos.LogServer.WebApiApplication" Language="C#" %>
